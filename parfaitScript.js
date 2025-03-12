33e464f15212a0309668f0c5f89682e8dd2a5580
doublestuforeyo <blueberriibiz@gmail.com>
2025-03-12 15:31:43 -0500

    added about me + modified parfait page

--- parfaitScript.js
+++ parfaitScript.js
@@ -3,11 +3,16 @@ let young = "https://i.pinimg.com/736x/21/cd/4d/21cd4d2aa5101e8c3f18ce608922683f
 let chill = "file:///Users/zariyascullark/Documents/Screenshot%202025-03-12%20at%2011.01.30%E2%80%AFAM.png";
 let standard = "https://i.pinimg.com/736x/0a/3d/16/0a3d16582aca3826606564f383e270ce.jpg"
 
-let costumes = [cherry, young, chill, standard];
+let costumes = [standard, cherry, young, chill];
 
-
+let num = 0;
 function changeImg(){
-    let randomIndex = Math.floor(Math.random()*4);
-    console.log(randomIndex);
-    document.getElementById('img1').src = costumes[randomIndex];
+    //let randomIndex = Math.floor(Math.random()*4);
+    num++;
+    if (num >= 4){
+        num = 0;
+    }
+
+    console.log(num);
+    document.getElementById('img1').src = costumes[num];
 }
