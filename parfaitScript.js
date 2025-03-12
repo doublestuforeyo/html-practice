9997ba838c440ddac2b9578fe30c15ff667d29f6
doublestuforeyo <blueberriibiz@gmail.com>
2025-03-12 11:12:47 -0500

    added random costume clicker

---
+++ parfaitScript.js
@@ -0,0 +1,13 @@
+let cherry = "https://i.pinimg.com/originals/66/ce/29/66ce29d34f1c66490cb8aa10c8b9f2cd.jpg";
+let young = "https://i.pinimg.com/736x/21/cd/4d/21cd4d2aa5101e8c3f18ce608922683f.jpg";
+let chill = "file:///Users/zariyascullark/Documents/Screenshot%202025-03-12%20at%2011.01.30%E2%80%AFAM.png";
+let standard = "https://i.pinimg.com/736x/0a/3d/16/0a3d16582aca3826606564f383e270ce.jpg"
+
+let costumes = [cherry, young, chill, standard];
+
+
+function changeImg(){
+    let randomIndex = Math.floor(Math.random()*4);
+    console.log(randomIndex);
+    document.getElementById('img1').src = costumes[randomIndex];
+}
